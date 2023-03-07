0c61331adbdf7fa2ad51ddb1030e0bb25a5c4fc5
Aedan-Vongsouthi-Student <112557510+Aedan-Vongsouthi-Student@users.noreply.github.com>
2023-03-07 10:01:10 -0500

    work on commit names

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    bool hasPackage;
+
     void OnCollisionEnter2D(Collision2D other) 
     {
         Debug.Log("Ge out te road mate befo aye shank ya with meh knofe!");
@@ -12,11 +14,14 @@ public class Delivery : MonoBehaviour
     void OnTriggerEnter2D(Collider2D other) {
       if (other.tag == "Package"){
         Debug.Log("Amongus");
-      }
+        hasPackage = true;
+        Destroy(other.gameObject, 0.5f)
+;      }
 
-      if (other.tag == "Customer")
+      if (other.tag == "Customer" && hasPackage == true)
       {
         Debug.Log("Shmmungus Delivered");
+        hasPackage = false;
       }
     }
 
